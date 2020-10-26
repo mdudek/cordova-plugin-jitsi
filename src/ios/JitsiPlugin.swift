7a8df961f29ab8975aeabced3e75dea333f7aedc
momoterraw <andrewvmail@gmail.com>
2020-10-26 14:57:16 -0700

    add in all the other events in the old obj c

--- src/ios/JitsiPlugin.swift
+++ src/ios/JitsiPlugin.swift
@@ -2,31 +2,28 @@ import Foundation
 import AVFoundation
 import JitsiMeet
 
-@objc(JitsiPlugin)
-class JitsiPlugin : CDVPlugin {
+class JitsiPlugin : CDVPlugin, JitsiMeetViewDelegate {
     
-    var pluginResult: CDVPluginResult!
     var jitsiMeetView: JitsiMeetView!;
+    var callbackId: String!
     fileprivate var pipViewCoordinator: PiPViewCoordinator?
-    
-    override init() { }
-    @objc(pluginInitialize) override func pluginInitialize() { NSLog("JitsiPlugin#pluginInitialize()") }
-    
+
     fileprivate func cleanUp() {
         jitsiMeetView?.removeFromSuperview()
         jitsiMeetView = nil
         pipViewCoordinator = nil
     }
-    
+
     @objc(join:) func join(_ command: CDVInvokedUrlCommand) {
-        NSLog("JitsiPlugin#join()")
+        callbackId = command.callbackId
         let serverUrl:String = command.arguments[0] as! String
         let room: String = command.arguments[1] as! String
         let isAudioOnly: Bool = command.arguments[2] as! Bool
+        
         jitsiMeetView = JitsiMeetView.init(frame: self.viewController.view.frame)
         jitsiMeetView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         jitsiMeetView.delegate = self
-        
+
         let options = JitsiMeetConferenceOptions.fromBuilder { (builder) in
             builder.welcomePageEnabled = false
             builder.serverURL = URL(string: serverUrl)
@@ -43,20 +40,83 @@ class JitsiPlugin : CDVPlugin {
         pipViewCoordinator?.configureAsStickyView(withParentView: viewController.view)
     }
 
-}
+    @objc(destroy:) func destroy(_ command: CDVInvokedUrlCommand) {
+        if((jitsiMeetView) != nil) {
+            jitsiMeetView.removeFromSuperview()
+            jitsiMeetView = nil
+        }
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "DESTROYED")
+        self.emit(callbackId, result: pluginResult!)
+    }
+    
+    func _onJitsiMeetViewDelegateEvent(name: NSString, _ data: [AnyHashable : Any]!) {
+            NSLog(
+                "[%s:%d] JitsiMeetViewDelegate %@ %@",
+                #file, #line, name, data);
+    }
+    
+    func conferenceFailed(_ data: [AnyHashable : Any]!) {
+        if((jitsiMeetView) != nil) {
+            jitsiMeetView.removeFromSuperview()
+            jitsiMeetView = nil
+        }
 
-extension JitsiPlugin: JitsiMeetViewDelegate {
+        _onJitsiMeetViewDelegateEvent(name: "CONFERENCE_FAILED", data);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "CONFERENCE_FAILED")
+        pluginResult!.setKeepCallbackAs(true)
+        self.emit(callbackId, result: pluginResult!)
+    }
+
+    func conferenceJoined(_ data: [AnyHashable : Any]!) {
+        _onJitsiMeetViewDelegateEvent(name: "CONFERENCE_JOINED", data);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "CONFERENCE_JOINED")
+        pluginResult!.setKeepCallbackAs(true)
+        self.emit(callbackId, result: pluginResult!)
+    }
+
+    func conferenceLeft(_ data: [AnyHashable : Any]!) {
+        _onJitsiMeetViewDelegateEvent(name: "CONFERENCE_LEFT", data);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "CONFERENCE_LEFT")
+        pluginResult!.setKeepCallbackAs(true)
+        self.emit(callbackId, result: pluginResult!)
+    }
+
+    func conferenceWillJoin(_ data: [AnyHashable : Any]!) {
+        _onJitsiMeetViewDelegateEvent(name: "CONFERENCE_WILL_JOIN", data);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "CONFERENCE_WILL_JOIN")
+        pluginResult!.setKeepCallbackAs(true)
+        self.emit(callbackId, result: pluginResult!)
+    }
+
     func conferenceTerminated(_ data: [AnyHashable : Any]!) {
+        _onJitsiMeetViewDelegateEvent(name: "CONFERENCE_TERMINATED", data);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "CONFERENCE_TERMINATED")
+        pluginResult!.setKeepCallbackAs(true)
+        self.emit(callbackId, result: pluginResult!)
+
         DispatchQueue.main.async {
             self.pipViewCoordinator?.hide() { _ in
                 self.cleanUp()
             }
         }
     }
-
+    
+    private func loadConfigError(_ data: [AnyHashable : Any]!) {
+        _onJitsiMeetViewDelegateEvent(name: "LOAD_CONFIG_ERROR", data);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "LOAD_CONFIG_ERROR")
+        pluginResult!.setKeepCallbackAs(true)
+        self.commandDelegate.send(pluginResult, callbackId: callbackId)
+    }
+    
     func enterPicture(inPicture data: [AnyHashable : Any]!) {
         DispatchQueue.main.async {
             self.pipViewCoordinator?.enterPictureInPicture()
+        }
+    }
+    
+    fileprivate func emit(_ callbackId: String, result: CDVPluginResult) {
+        DispatchQueue.main.async {
+            self.commandDelegate!.send(result, callbackId: callbackId)
         }
     }
 }
